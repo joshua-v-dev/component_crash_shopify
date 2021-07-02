71532cbb63a913ed997b889e574d4f8850f4ce7c
joshua-v-dev <joshua-v-dev@gmail.com>
2021-07-02 02:02:55 -0400

    added syntax for polaris on page element index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
-  <Page>
+  <Page
+    title="Product Selector"
+    primaryAction={{
+      content: "Select product",
+      onAction: () => console.log("I have been clicked"),
+    }}
+  >
     <Heading>Joshua's First Shopify app with Node and React 🎉</Heading>
   </Page>
 );
