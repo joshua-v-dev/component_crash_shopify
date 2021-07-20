3c44d54aa458ed0a0e1caebad4a9612293286457
joshua-v-dev <joshua-v-dev@gmail.com>
2021-07-20 02:16:49 -0400

    node engine specified package.json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
 const { parsed: localEnv } = require("dotenv").config();
 
-const webpack = require("webpack");
+import { DefinePlugin } from "webpack";
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
-module.exports = {
-  webpack: (config) => {
-    const env = { API_KEY: apiKey };
-    config.plugins.push(new webpack.DefinePlugin(env));
+export function webpack(config) {
+  const env = { API_KEY: apiKey };
+  config.plugins.push(new DefinePlugin(env));
 
-    // Add ESM support for .mjs files in webpack 4
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    });
+  // Add ESM support for .mjs files in webpack 4
+  config.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
 
-    return config;
-  },
-};
+  return config;
+}
